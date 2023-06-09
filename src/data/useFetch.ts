bab4fd89221d7315f31208df6e8cdd96373ff6fa
Somin <sominupadhyaya@gmail.com>
2023-06-09 21:52:54 +0545

    complete useFetch hook

--- src/data/useFetch.ts
+++ src/data/useFetch.ts
@@ -1,7 +1,12 @@
 import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
 
 const useFetch = (URL:string) =>{
-    return URL
+    const {isLoading, error, data} = useQuery({
+        queryKey : ['lyricsData'],
+        queryFn : () => axios(URL)
+    })
+    return {isLoading,error,data}
 }
 
 export default useFetch
