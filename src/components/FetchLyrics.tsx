a6819480dd13df3de1112ff3406e2c652afad89c
Somin <sominupadhyaya@gmail.com>
2023-06-12 18:21:37 +0545

    add the dummy fetch URL and encounter CORS issue

--- src/components/FetchLyrics.tsx
+++ src/components/FetchLyrics.tsx
@@ -1,8 +1,10 @@
 import useFetch from "../data/useFetch"
+import { ROOT_URL,API_KEY } from "../data/client"
 
 const FetchLyrics = () => {
+  const fetchURL = "track.lyrics.get?track_id=15953433"
   const { isLoading, data, error } = useFetch(
-    
+    `${ROOT_URL}/${fetchURL}&apikey=${API_KEY}`
   )
   return (
     <div>FetchLyrics</div>
